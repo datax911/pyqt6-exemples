bb406ae66de7f44019330f7232900ccc0de97899
vinny <commutateur@gmail.com>
2025-02-22 16:12:52 -0500

    kewl

---
+++ QLineEdit.py
@@ -0,0 +1,21 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit
+from PyQt6.QtGui import QIcon, QFont
+import sys
+
+class Window(QWidget):
+	def __init__(self):
+		super().__init__()
+
+		self.setGeometry(200,200,700,400)
+		self.setWindowTitle("PyQt6 QPushButton")
+		self.setWindowIcon(QIcon('images/python.png'))
+
+		line_edit = QLineEdit(self)
+		line_edit.setFont(QFont("Sanserif", 15))
+		line_edit.setPlaceholderText("Please enter your username.")
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
